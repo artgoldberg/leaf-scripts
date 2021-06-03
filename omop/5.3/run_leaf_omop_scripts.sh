b1171e9521270d4fd65adf56ce360ca4f34f7eb9
Arthur Goldberg <artgoldberg@gmail.com>
2021-06-03 15:58:46 -0400

    drop port

--- omop/5.3/run_leaf_omop_scripts.sh
+++ omop/5.3/run_leaf_omop_scripts.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 # comment: use command ./run_leaf_omop_scripts.sh
 # run the leaf-scripts
-MSDW2_PROD="10.95.46.175:1433"
+MSDW2_PROD="10.95.46.175"
 LEAF_SVC_UID="sql_svc_leaf"
 # LEAF_SVC_PWD must be defined in the shell environment
 sqlcmd -S $MSDW2_PROD -U $LEAF_SVC_UID -P $LEAF_SVC_PWD -i 2_demographics.sql
