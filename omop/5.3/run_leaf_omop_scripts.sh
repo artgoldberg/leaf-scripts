1d64a1a86d5cbc5db4addd0e7a39bb01659d0b7c
Arthur Goldberg <artgoldberg@gmail.com>
2021-06-03 14:45:27 -0400

    start writing run_leaf_omop_scripts.sh

--- omop/5.3/run_leaf_omop_scripts.sh
+++ omop/5.3/run_leaf_omop_scripts.sh
@@ -1,1 +1,5 @@
 # run the leaf-scripts
+MSDW2_PROD="10.95.46.175:1433"
+LEAF_SVC_UID="sql_svc_leaf"
+# LEAF_SVC_PWD must be defined in the shell environment
+sqlcmd -S $MSDW2_PROD -U $LEAF_SVC_UID -P $LEAF_SVC_PWD -i 2_demographics.sql
