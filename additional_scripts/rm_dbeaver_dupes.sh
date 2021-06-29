a7e9c8fa83face19f701f774f4461a251de11878
Arthur Goldberg <artgoldberg@gmail.com>
2021-06-28 20:39:16 -0400

    add more selective data about common source codes

--- additional_scripts/rm_dbeaver_dupes.sh
+++ additional_scripts/rm_dbeaver_dupes.sh
@@ -1,3 +1,3 @@
-awk '!seen[$0]++' common_source_codes.csv > tmp/common_source_codes.csv
-mv tmp/common_source_codes.csv common_source_codes.csv
+awk '!seen[$0]++' common_source_codes.csv > /tmp/common_source_codes.csv
+mv /tmp/common_source_codes.csv common_source_codes.csv
 echo "DBeaver dupes removed from common_source_codes.csv"
