f3226c62a11dd869fdbb74a2f4b7bb31f20a8043
Arthur Goldberg <artgoldberg@gmail.com>
2021-07-19 17:30:36 -0400

    modify for use by MSDW2; add meaningful field names to result

--- leaf_launch/conditions/map_ICD10_to_SNOMED.sql
+++ leaf_launch/conditions/map_ICD10_to_SNOMED.sql
@@ -1,16 +1,19 @@
 -- Map ICD-10-CM to SNOMED
 
--- Result is a set of CONCEPT_ID pairs, ICD-10-CM concept id and SNOMED concept id, plus
--- their CONCEPT_NAMEs to help review.
+-- Result is a set of CONCEPT_ID pairs, mapping a ICD-10-CM concept id to a SNOMED concept id,
+-- and including their CONCEPT_NAMEs to help review.
 -- Approach: get records in CONCEPT_RELATIONSHIP that map from ICD-10-CM to SNOMED.
 
-USE leaf_setup;
+USE omop;
 
-SELECT c1.CONCEPT_ID, c2.CONCEPT_ID, c1.CONCEPT_NAME, c2.CONCEPT_NAME
-FROM athena.CONCEPT_RELATIONSHIP cr
-    , athena.CONCEPT c1
-    , athena.CONCEPT c2    
-WHERE 
+SELECT c1.CONCEPT_ID as ICD10CM_CONCEPT_ID
+    , c2.CONCEPT_ID as SNOMED_CONCEPT_NAME
+    , c1.CONCEPT_NAME as ICD10CM_CONCEPT_NAME
+    , c2.CONCEPT_NAME as SNOMED_CONCEPT_NAME
+FROM cdm_std.CONCEPT_RELATIONSHIP cr
+    , cdm_std.CONCEPT c1
+    , cdm_std.CONCEPT c2
+WHERE
     c1.VOCABULARY_ID = 'ICD10CM' AND
     c2.VOCABULARY_ID = 'SNOMED' AND
     cr.RELATIONSHIP_ID = 'Maps to' AND
@@ -18,13 +21,12 @@ WHERE
     c2.CONCEPT_ID = cr.CONCEPT_ID_2;
 
 SELECT COUNT(*)
-FROM athena.CONCEPT_RELATIONSHIP cr
-    , athena.CONCEPT c1
-    , athena.CONCEPT c2    
-WHERE 
+FROM cdm_std.CONCEPT_RELATIONSHIP cr
+    , cdm_std.CONCEPT c1
+    , cdm_std.CONCEPT c2
+WHERE
     c1.VOCABULARY_ID = 'ICD10CM' AND
     c2.VOCABULARY_ID = 'SNOMED' AND
     cr.RELATIONSHIP_ID = 'Maps to' AND
     c1.CONCEPT_ID = cr.CONCEPT_ID_1 AND
     c2.CONCEPT_ID = cr.CONCEPT_ID_2;
-
