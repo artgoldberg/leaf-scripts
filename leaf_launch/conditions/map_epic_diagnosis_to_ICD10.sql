9ec4257aa7239e7901de77661bdd12a171e3afa0
Arthur Goldberg <artgoldberg@gmail.com>
2021-07-21 17:35:48 -0400

    map to SNOMED instead

--- leaf_launch/conditions/map_epic_diagnosis_to_ICD10.sql
+++ leaf_launch/conditions/map_epic_diagnosis_to_ICD10.sql
@@ -1,22 +1,22 @@
--- Get 'Epic diagnosis ID' → ICD-10-CM mappings by Sharon from concept_relationship
+-- Get 'Epic diagnosis ID' → SNOMED mappings by Sharon from concept_relationship
 
--- Result is a set of CONCEPT_ID pairs, mapping an 'Epic diagnosis ID' to a ICD-10-CM concept id, along with their CONCEPT_NAMEs to help review.
+-- Result is a set of CONCEPT_ID pairs, mapping an 'Epic diagnosis ID' to a SNOMED concept id, along with their CONCEPT_NAMEs to help review.
 
 
--- Approach: get records in CONCEPT_RELATIONSHIP that map from 'EPIC EDG .1' to 'ICD10CM'
+-- Approach: get records in CONCEPT_RELATIONSHIP that map from 'EPIC EDG .1' to 'SNOMED'
 
 USE omop;
 
 SELECT c_source.CONCEPT_ID as EPIC_DIAG_ID
-    , c_dest.CONCEPT_ID as ICD10CM_CONCEPT_ID
     , c_source.CONCEPT_NAME as EPIC_DIAG_NAME
-    , c_dest.CONCEPT_NAME as ICD10CM_CONCEPT_NAME
+    , c_dest.CONCEPT_ID as SNOMED_CONCEPT_ID
+    , c_dest.CONCEPT_NAME as SNOMED_CONCEPT_NAME
 FROM cdm_std.CONCEPT_RELATIONSHIP cr
     , cdm_std.CONCEPT c_source
     , cdm_std.CONCEPT c_dest
 WHERE
     c_source.VOCABULARY_ID = 'EPIC EDG .1' AND
-    c_dest.VOCABULARY_ID = 'ICD10CM' AND
     cr.RELATIONSHIP_ID = 'Maps to' AND
+    c_dest.VOCABULARY_ID = 'SNOMED' AND
     c_source.CONCEPT_ID = cr.CONCEPT_ID_1 AND
     c_dest.CONCEPT_ID = cr.CONCEPT_ID_2;
