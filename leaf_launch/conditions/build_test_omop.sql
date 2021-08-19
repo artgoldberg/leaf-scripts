687d15657763606c04adc9980fe5096c21eb4fe5
Arthur Goldberg <artgoldberg@gmail.com>
2021-08-19 13:33:26 -0400

    o report on percent of condition_occurrence records with condition_concept_id set o examine entire condition_occurrence table o simplify query that sets condition_occurrence.condition_concept_id by using new fields in leaf_scratch.conditions_map o clarify comments

--- leaf_launch/conditions/build_test_omop.sql
+++ leaf_launch/conditions/build_test_omop.sql
@@ -7,33 +7,16 @@
 Must be executed as goldba06@MSNYUHEALTH.ORG.
 
 Steps:
-1. create, load and index tables I need to modify: concept_relationship & condition_occurrence
-2. update concept_relationship & condition_occurrence from leaf_scratch.conditions_map
+1. create, load and index the table I need to modify: condition_occurrence
+2. update condition_occurrence from Leaf_scratch.conditions_map
 3. create record in LeafDB.app.ConceptSqlSet for this test condition_occurrence
-4. revise leaf_icd10.sql to use these tables
-5. run and test leaf_icd10.sql
+4. run and test leaf_icd10.sql
 */
 
 -- 1. create, load and index tables I need to modify: concept_relationship & condition_occurrence
 PRINT 'Starting ''build_test_omop.sql'' at ' + CONVERT(VARCHAR, GETDATE(), 120)
 
 USE rpt
-
-/*
-Not needed:
-
-IF (EXISTS (SELECT *
-            FROM information_schema.tables
-            WHERE table_schema = 'test_omop_conditions'
-            AND table_name = 'concept_relationship'))
-BEGIN
-    DROP TABLE test_omop_conditions.concept_relationship
-END
-
-SELECT TOP 1000000 *
-INTO test_omop_conditions.concept_relationship
-FROM omop.cdm_std.concept_relationship
-*/
 
 IF (EXISTS (SELECT *
             FROM information_schema.tables
@@ -43,7 +26,8 @@ BEGIN
     DROP TABLE test_omop_conditions.condition_occurrence
 END
 
-SELECT TOP 1000000 *
+-- 100,000,000 = all
+SELECT TOP 100000000 *
 INTO test_omop_conditions.condition_occurrence
 FROM omop.cdm_std.condition_occurrence
 
@@ -84,90 +68,46 @@ IF NOT EXISTS(SELECT *
         ON test_omop_conditions.condition_occurrence (condition_source_concept_id)
     END;
 
-PRINT '1: Completed loading and indexing'
+PRINT '1: Completed loading of condition_occurrence and indexing of it and conditions_map'
 
+DECLARE @num_condition_occurrence_records INT = (SELECT COUNT(*)
+                                                 FROM test_omop_conditions.condition_occurrence)
+PRINT CAST(@num_condition_occurrence_records AS VARCHAR) +
+    ' records in test_omop_conditions.condition_occurrence'
 
--- 2. update concept_relationship & condition_occurrence from leaf_scratch.conditions_map
--- insert conditions_map SNOMED to ICD10 relationships into concept_relationship
-    -- ICD10 'Maps to' SNOMED required
-/*
--- SELECT to check the INSERT below
-SELECT DISTINCT concept_ICD10.concept_id ICD10_concept_id,
-                concept_ICD10.concept_code ICD10_concept_code,
-                concept_ICD10.concept_name ICD10_concept_name,
-                concept_SNOMED.concept_id SNOMED_concept_id,
-                concept_SNOMED.concept_code SNOMED_concept_code,
-                concept_SNOMED.concept_name SNOMED_concept_name
-FROM leaf_scratch.conditions_map conditions_map,
-     omop.cdm_std.concept concept_ICD10,
-     omop.cdm_std.concept concept_SNOMED    
-WHERE concept_ICD10.vocabulary_id = 'ICD10CM'
-      AND concept_ICD10.concept_code = conditions_map.ICD10_concept_code
-      AND concept_SNOMED.vocabulary_id = 'SNOMED'
-      AND concept_SNOMED.concept_code = conditions_map.SNOMED_concept_code
-
-Not needed, as the ICD10 -> SNOMED relationships are already in concept_relationship
-INSERT INTO test_omop_conditions.concept_relationship(concept_id_1,
-                                                      concept_id_2,
-                                                      relationship_id,
-                                                      valid_start_date,
-                                                      valid_end_date)
-    SELECT DISTINCT concept_ICD10.concept_id,
-                    concept_SNOMED.concept_id,
-                    'Maps to',
-                    CONVERT(DATE, GETDATE()),
-                    DATEADD(YEAR, 10, CONVERT(DATE, GETDATE()))
-    FROM leaf_scratch.conditions_map conditions_map,
-         omop.cdm_std.concept concept_ICD10,
-         omop.cdm_std.concept concept_SNOMED         
-    WHERE concept_ICD10.vocabulary_id = 'ICD10CM'
-          AND concept_ICD10.concept_code = conditions_map.ICD10_concept_code
-          AND concept_SNOMED.vocabulary_id = 'SNOMED'
-          AND concept_SNOMED.concept_code = conditions_map.SNOMED_concept_code;
-
-PRINT 'Completed update concept_relationship & condition_occurrence from leaf_scratch.conditions_map'
-*/
+-- 2. update condition_occurrence from leaf_scratch.conditions_map
 
 -- update condition_occurrence fields with conditions_map SNOMED info:
 /*
-Query for testing UPDATE below
-SELECT condition_occurrence.condition_occurrence_id,
-       concept_SNOMED.concept_id SNOMED_concept_id,
-       concept_SNOMED.concept_code SNOMED_concept_code,
-       concept_SNOMED.concept_name SNOMED_concept_name,
-       concept_Epic.concept_code Epic_concept_code,
-       concept_Epic.concept_name Epic_concept_name       
-FROM test_omop_conditions.condition_occurrence condition_occurrence,
-     leaf_scratch.conditions_map conditions_map,
-     omop.cdm_std.concept concept_Epic,
-     omop.cdm_std.concept concept_SNOMED
-WHERE NOT condition_occurrence.condition_source_concept_id = 0
-      AND condition_occurrence.condition_source_concept_id = concept_Epic.concept_id
-      AND concept_Epic.concept_code = conditions_map.Epic_concept_code
-      AND concept_Epic.vocabulary_id = 'EPIC EDG .1'
-      AND conditions_map.SNOMED_concept_code = concept_SNOMED.concept_code
-      AND concept_SNOMED.vocabulary_id = 'SNOMED'
-ORDER BY concept_SNOMED.concept_id,
-       concept_SNOMED.concept_code,
-       concept_SNOMED.concept_name,
-       concept_Epic.concept_code,
-       concept_Epic.concept_name
+-- Query for testing UPDATE below
+SELECT SNOMED_concept_id,
+       SNOMED_concept_code,
+       SNOMED_concept_name,
+       condition_source_concept_code Epic_concept_code,
+       condition_source_concept_name Epic_concept_name
+FROM test_omop_conditions.condition_occurrence,
+     leaf_scratch.conditions_map
+WHERE NOT condition_source_concept_id = 0
+      AND condition_source_concept_id = Epic_concept_id
 */
 
 UPDATE test_omop_conditions.condition_occurrence
-SET condition_concept_id = concept_SNOMED.concept_id,
-    condition_concept_code = concept_SNOMED.concept_code,
-    condition_concept_name = concept_SNOMED.concept_name
-FROM test_omop_conditions.condition_occurrence condition_occurrence,
-     leaf_scratch.conditions_map conditions_map,
-     omop.cdm_std.concept concept_Epic,
-     omop.cdm_std.concept concept_SNOMED
-WHERE NOT condition_occurrence.condition_source_concept_id = 0
-      AND condition_occurrence.condition_source_concept_id = concept_Epic.concept_id
-      AND concept_Epic.concept_code = conditions_map.Epic_concept_code
-      AND concept_Epic.vocabulary_id = 'EPIC EDG .1'
-      AND conditions_map.SNOMED_concept_code = concept_SNOMED.concept_code
-      AND concept_SNOMED.vocabulary_id = 'SNOMED'
+SET condition_concept_id = SNOMED_concept_id,
+    condition_concept_code = SNOMED_concept_code,
+    condition_concept_name = SNOMED_concept_name
+FROM test_omop_conditions.condition_occurrence,
+     leaf_scratch.conditions_map
+WHERE NOT condition_source_concept_id = 0
+      AND condition_source_concept_id = Epic_concept_id
+
+DECLARE @num_condition_occurrences_w_omop_concepts INT = (SELECT COUNT(*)
+                                                          FROM test_omop_conditions.condition_occurrence
+                                                          WHERE condition_concept_id <> 0)
+PRINT CAST(@num_condition_occurrences_w_omop_concepts AS VARCHAR) +
+    ' records in test_omop_conditions.condition_occurrence with condition_concept_id set'
+
+PRINT CAST(100 * @num_condition_occurrences_w_omop_concepts / @num_condition_occurrence_records AS VARCHAR) +
+    '% of condition_occurrence records have condition_concept_id set'
 
 PRINT 'Completed update condition_occurrence fields with conditions_map SNOMED info'
 
@@ -175,5 +115,3 @@ PRINT 'Completed update condition_occurrence fields with conditions_map SNOMED i
 
 PRINT 'Finishing ''build_test_omop.sql'' at ' + CONVERT(VARCHAR, GETDATE(), 120)
 PRINT ''
-
--- 5. run leaf_icd10.sql
