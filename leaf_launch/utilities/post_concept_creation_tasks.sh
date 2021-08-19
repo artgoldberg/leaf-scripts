df40dd30495d70b9b6f19b9c7aba3762e31b93f7
Arthur Goldberg <artgoldberg@gmail.com>
2021-08-19 18:00:31 -0400

    update for nohup

--- leaf_launch/utilities/post_concept_creation_tasks.sh
+++ leaf_launch/utilities/post_concept_creation_tasks.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # run the Leaf stored procedures after concepts are created and/or updated
 # password must be entered manually
+# run with nohup
 
-echo "$(date '+%Y-%m-%d %H:%M'): starting 'post_concept_creation_tasks.sh'"
-sqlcmd -S msdw2-mssql-prd.msnyuhealth.org -U sql_svc_leaf -i post_concept_creation_tasks.sql
-echo "$(date '+%Y-%m-%d %H:%M'): finished 'post_concept_creation_tasks.sh'"
+echo "$(date '+%Y-%m-%d %H:%M'): starting 'post_concept_creation_tasks.sh'" >> post_concept_creation_tasks.log
+sqlcmd -S msdw2-mssql-prd.msnyuhealth.org -U sql_svc_leaf -i post_concept_creation_tasks.sql >> post_concept_creation_tasks.log
+echo "$(date '+%Y-%m-%d %H:%M'): finished 'post_concept_creation_tasks.sh'" >> post_concept_creation_tasks.log
