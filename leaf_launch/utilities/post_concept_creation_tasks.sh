70ceac2c9c91e84b7655f4904076d616348653ef
Arthur Goldberg <artgoldberg@gmail.com>
2021-08-20 17:08:55 -0400

    also log errors

--- leaf_launch/utilities/post_concept_creation_tasks.sh
+++ leaf_launch/utilities/post_concept_creation_tasks.sh
@@ -4,5 +4,7 @@
 # run using "nohup ./post_concept_creation_tasks.sh &"
 
 echo "$(date '+%Y-%m-%d %H:%M'): starting 'post_concept_creation_tasks.sh'" >> post_concept_creation_tasks.log
-sqlcmd -S msdw2-mssql-prd.msnyuhealth.org -U sql_svc_leaf -i ../../omop/5.3/post_concept_creation_tasks.sql >> post_concept_creation_tasks.log
+echo "$(date '+%Y-%m-%d %H:%M'): starting 'post_concept_creation_tasks.sh'" >> post_concept_creation_tasks_error.log
+sqlcmd -S msdw2-mssql-prd.msnyuhealth.org -U sql_svc_leaf -i ../../omop/5.3/post_concept_creation_tasks.sql >> post_concept_creation_tasks.log 2>> post_concept_creation_tasks_error.log
 echo "$(date '+%Y-%m-%d %H:%M'): finished 'post_concept_creation_tasks.sh'" >> post_concept_creation_tasks.log
+echo "$(date '+%Y-%m-%d %H:%M'): finished 'post_concept_creation_tasks.sh'" >> post_concept_creation_tasks_error.log
