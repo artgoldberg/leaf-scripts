5ace3ebeee1d6cc955547c6b575d6545cc7170be
Arthur Goldberg <artgoldberg@gmail.com>
2021-08-25 12:44:21 -0400

    add Procedure codes

--- leaf_launch/utilities/MSDW2_QC/data_needed_by_Leaf.sql
+++ leaf_launch/utilities/MSDW2_QC/data_needed_by_Leaf.sql
@@ -191,3 +191,35 @@ FROM cdm_deid.person;
 
 SELECT COUNT(DISTINCT person_id)
 FROM cdm_deid.person;
+
+-- Procedure codes
+SELECT 'cdm' AS 'Schema',
+       'Procedures' AS 'Code',
+       COUNT(*) AS 'Count',
+       procedure_source_concept_id,
+       '',
+       '',
+       procedure_source_value
+FROM cdm.procedure_occurrence
+WHERE procedure_source_concept_id IS NOT NULL
+      AND 0 < procedure_source_concept_id
+      AND procedure_source_value IS NOT NULL
+GROUP BY procedure_source_concept_id, procedure_source_value;
+
+SELECT 'cdm_std' AS 'Schema',
+       'Procedures' AS 'Code',
+       COUNT(*) AS 'Count',
+       procedure_source_concept_id,
+       procedure_source_concept_code,
+       procedure_source_concept_name,
+       procedure_source_value
+FROM cdm_std.procedure_occurrence
+WHERE procedure_source_concept_id IS NOT NULL
+      AND 0 < procedure_source_concept_id
+      AND procedure_source_concept_code IS NOT NULL
+      AND procedure_source_concept_name IS NOT NULL
+      AND procedure_source_value IS NOT NULL
+GROUP BY procedure_source_concept_id,
+         procedure_source_concept_code,
+         procedure_source_concept_name,
+         procedure_source_value
