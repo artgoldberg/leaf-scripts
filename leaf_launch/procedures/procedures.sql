17dd4cd9f125f59490ebe340c10b674c5faf63aa
Arthur Goldberg <artgoldberg@gmail.com>
2021-08-26 17:06:15 -0400

    o in tims... get procedure code mappings from ProcedureDim o draft initial version of procedure.sql

--- leaf_launch/procedures/procedures.sql
+++ leaf_launch/procedures/procedures.sql
@@ -20,4 +20,157 @@ Steps
 4. Insert new mappings into rpt.Leaf_usagi.Leaf_staging
 */
 
+-- 0. Create procedures_map table
 
+PRINT 'Starting ''procedures.sql'' at ' + CONVERT(VARCHAR, GETDATE(), 120)
+
+USE rpt;
+
+IF (NOT EXISTS (SELECT *
+                FROM information_schema.tables
+                WHERE table_schema = 'leaf_scratch'
+                AND table_name = 'procedures_map'))
+    BEGIN
+        CREATE TABLE leaf_scratch.procedures_map
+        (
+            Epic_concept_id INT NOT NULL PRIMARY KEY,
+            Epic_concept_code NVARCHAR(50),
+            Epic_concept_name NVARCHAR(255) NOT NULL,
+            CPT4_concept_id INT,
+            CPT4_concept_code NVARCHAR(50) NOT NULL UNIQUE,
+            CPT4_concept_name NVARCHAR(255) NOT NULL,
+            -- Relationship of Sharon's hand-coded Epic -> CPT4 mapping to automated mapping
+            hand_map_status NVARCHAR(50),
+            sources NVARCHAR(200) NOT NULL,     -- Sources for a record
+            comment NVARCHAR(200)
+        )
+    END
+ELSE
+    DELETE FROM leaf_scratch.procedures_map
+
+
+-- 1. Map 'Epic procedure codes' to CPT, from the Epic concept in src.caboodle.ProcedureDim
+USE src;
+
+-- DROP temp tables
+IF OBJECT_ID(N'tempdb..#proc_mappings_from_code') IS NOT NULL
+	DROP TABLE #proc_mappings_from_code
+IF OBJECT_ID(N'tempdb..#proc_mappings_from_cpt_code') IS NOT NULL
+	DROP TABLE #proc_mappings_from_cpt_code
+
+-- Get mappings from Code
+SELECT procedure_dim.ProcedureEpicId,
+       procedure_dim.[Name] AS 'Epic_name',
+       concept.concept_name,
+       procedure_dim.Code AS 'Code'
+INTO #proc_mappings_from_code
+FROM caboodle.ProcedureDim procedure_dim,
+     omop.cdm.concept concept
+WHERE procedure_dim.IsCurrent = 1
+      AND SUBSTRING(procedure_dim.SurgicalProcedureEpicId, 4, 5) <> procedure_dim.Code
+      AND procedure_dim.Code != '*Unspecified'
+      AND procedure_dim.ProcedureEpicId IS NOT NULL
+      AND procedure_dim.CodeSet = 'CPT(R)'
+      -- These appear to be invalid, deleted or non-procedure codes
+      AND NOT (procedure_dim.[Name] LIKE '-----%'
+               OR procedure_dim.[Name] LIKE 'CODE DELETED FOR %'
+               OR procedure_dim.[Name] LIKE 'DELETED %'
+               OR procedure_dim.[Name] LIKE 'COPAYMENT % DOLLARS')
+      -- Avoid non-Clarity data added by Population Health
+      AND procedure_dim._HasSourceClarity = 1 AND procedure_dim._IsDeleted = 0
+      -- Ensure procedure_dim.Code is a valid CPT4 code
+      AND concept.vocabulary_id = 'CPT4'
+      AND concept.concept_code  = procedure_dim.Code
+GROUP BY procedure_dim.ProcedureEpicId,
+         procedure_dim.[Name],
+         concept.concept_name,
+         procedure_dim.Code;
+
+-- Get mappings directly from CptCode
+SELECT procedure_dim.ProcedureEpicId,
+       procedure_dim.[Name] AS 'Epic_name',
+       concept.concept_name,
+       procedure_dim.CptCode AS 'Code'
+INTO #proc_mappings_from_cpt_code
+FROM caboodle.ProcedureDim procedure_dim,
+     omop.cdm.concept concept
+WHERE procedure_dim.IsCurrent = 1
+      AND procedure_dim.ProcedureEpicId IS NOT NULL
+      AND NOT procedure_dim.CptCode IN ('', '*Not Applicable', '*Deleted', '*Unknown')
+      -- These appear to be invalid, deleted or non-procedure codes
+      AND NOT (procedure_dim.[Name] LIKE '-----%'
+               OR procedure_dim.[Name] LIKE 'CODE DELETED FOR %'
+               OR procedure_dim.[Name] LIKE 'DELETED %'
+               OR procedure_dim.[Name] LIKE 'COPAYMENT % DOLLARS')
+      -- Avoid non-Clarity data added by Population Health
+      AND procedure_dim._HasSourceClarity = 1 AND procedure_dim._IsDeleted = 0
+      -- Ensure procedure_dim.Code is a valid CPT4 code
+      AND concept.vocabulary_id = 'CPT4'
+      AND concept.concept_code  = procedure_dim.CptCode
+GROUP BY procedure_dim.ProcedureEpicId,
+         procedure_dim.[Name],
+         concept.concept_name,
+         procedure_dim.CptCode;
+
+-- Conflicting codes from the two methods, which can be reconciled or ignored
+-- Join on ProcedureEpicId, showing both Code and CptCode
+IF OBJECT_ID(N'tempdb..#conflicting_proc_mappings') IS NOT NULL
+	DROP TABLE #conflicting_proc_mappings;
+
+SELECT #proc_mappings_from_cpt_code.ProcedureEpicId,
+       #proc_mappings_from_cpt_code.Epic_name,
+       #proc_mappings_from_cpt_code.Code AS 'CptCode_code',
+       #proc_mappings_from_code.Code AS 'Code_code'
+INTO #conflicting_proc_mappings
+FROM #proc_mappings_from_cpt_code,
+     #proc_mappings_from_code
+WHERE #proc_mappings_from_cpt_code.ProcedureEpicId = #proc_mappings_from_code.ProcedureEpicId
+      AND #proc_mappings_from_cpt_code.Code <> #proc_mappings_from_code.Code;
+
+SELECT *
+FROM #conflicting_proc_mappings;
+
+-- Code mappings that do not conflict or are provided by only one method, which will be used as is
+IF OBJECT_ID(N'tempdb..#non_conflicting_proc_mappings') IS NOT NULL
+	DROP TABLE #non_conflicting_proc_mappings;
+
+USE rpt;
+
+-- Keep non-conflicting mappings from CptCode and Code, without duplication
+INSERT INTO leaf_scratch.procedures_map (Epic_concept_id,
+                                         Epic_concept_name,
+                                         CPT4_concept_name,
+                                         CPT4_concept_code,
+                                         sources)
+SELECT ProcedureEpicId,
+       Epic_name,
+       concept_name,
+       Code,
+       'caboodle.ProcedureDim'
+FROM (SELECT *
+      FROM #proc_mappings_from_cpt_code
+      WHERE ProcedureEpicId NOT IN (SELECT ProcedureEpicId
+                                    FROM #conflicting_proc_mappings)
+
+      UNION
+
+      SELECT *
+      FROM #proc_mappings_from_code
+      WHERE ProcedureEpicId NOT IN (SELECT ProcedureEpicId
+                                    FROM #conflicting_proc_mappings)) proc_mappings;
+
+UPDATE leaf_scratch.procedures_map
+SET Epic_concept_code = Epic_concept.concept_code,
+    CPT4_concept_id = CPT4_concept.concept_id
+FROM omop.cdm.concept Epic_concept,
+     omop.cdm.concept CPT4_concept,
+     leaf_scratch.procedures_map procedures_map
+WHERE procedures_map.Epic_concept_id = Epic_concept.concept_id
+      AND procedures_map.CPT4_concept_code = CPT4_concept.concept_code
+
+/*
+read sharon's procedure mappings into a table_name
+incorporate them into procedures_map
+have Sharon review
+*/
+PRINT 'Finishing ''procedures.sql'' at ' + CONVERT(VARCHAR, GETDATE(), 120)
--- leaf_launch/procedures/tims_eval_of_codes.sql
+++ leaf_launch/procedures/tims_eval_of_codes.sql
@@ -5,59 +5,106 @@
 
 USE src;
 
+-- DROP temp tables
+IF OBJECT_ID(N'tempdb..#proc_mappings_from_code') IS NOT NULL
+	DROP TABLE #proc_mappings_from_code
+IF OBJECT_ID(N'tempdb..#proc_mappings_from_cpt_code') IS NOT NULL
+	DROP TABLE #proc_mappings_from_cpt_code
+
+-- Get mappings from Code
 SELECT procedure_dim.ProcedureEpicId,
-       procedure_dim.[Name],
-       procedure_dim.Code,
-       procedure_dim.CodeSet,
-       COUNT(*) [count]
-FROM caboodle.ProcedureDim procedure_dim
+       procedure_dim.[Name] AS 'Epic_name',
+       concept.concept_name,
+       procedure_dim.Code AS 'Code'
+INTO #proc_mappings_from_code
+FROM caboodle.ProcedureDim procedure_dim,
+     omop.cdm.concept concept
 WHERE procedure_dim.IsCurrent = 1
       AND SUBSTRING(procedure_dim.SurgicalProcedureEpicId, 4, 5) <> procedure_dim.Code
       AND procedure_dim.Code != '*Unspecified'
       AND procedure_dim.ProcedureEpicId IS NOT NULL
-      -- These appear to be invalid, deleted or non-procedure codes
-      AND NOT procedure_dim.[Name] LIKE '-----%'
-      AND NOT procedure_dim.[Name] LIKE 'CODE DELETED FOR %'
-      AND NOT procedure_dim.[Name] LIKE 'COPAYMENT % DOLLARS'
-      AND NOT procedure_dim.[Name] LIKE 'DELETED %'
       AND procedure_dim.CodeSet = 'CPT(R)'
--- todo: add the Avoid non-Clarity data added by Population Health condition
+      -- These appear to be invalid, deleted or non-procedure codes
+      AND NOT (procedure_dim.[Name] LIKE '-----%'
+               OR procedure_dim.[Name] LIKE 'CODE DELETED FOR %'
+               OR procedure_dim.[Name] LIKE 'DELETED %'
+               OR procedure_dim.[Name] LIKE 'COPAYMENT % DOLLARS')
+      -- Avoid non-Clarity data added by Population Health
+      AND procedure_dim._HasSourceClarity = 1 AND procedure_dim._IsDeleted = 0
+      -- Ensure procedure_dim.Code is a valid CPT4 code
+      AND concept.vocabulary_id = 'CPT4'
+      AND concept.concept_code  = procedure_dim.Code
 GROUP BY procedure_dim.ProcedureEpicId,
          procedure_dim.[Name],
-         procedure_dim.Code,
-         procedure_dim.CodeSet;
+         concept.concept_name,
+         procedure_dim.Code;
 
--- Try to get mappings directly from CptCode
+-- Get mappings directly from CptCode
 SELECT procedure_dim.ProcedureEpicId,
-       procedure_dim.[Name],
-       procedure_dim.CptCode,
-       'CPT(R)' AS 'CodeSet',
-       COUNT(*) [count]
-FROM caboodle.ProcedureDim procedure_dim
+       procedure_dim.[Name] AS 'Epic_name',
+       concept.concept_name,
+       procedure_dim.CptCode AS 'Code'
+INTO #proc_mappings_from_cpt_code
+FROM caboodle.ProcedureDim procedure_dim,
+     omop.cdm.concept concept
 WHERE procedure_dim.IsCurrent = 1
       AND procedure_dim.ProcedureEpicId IS NOT NULL
-      -- These appear to be invalid, deleted or non-procedure codes
-      AND NOT procedure_dim.[Name] LIKE '-----%'
-      AND NOT procedure_dim.[Name] LIKE 'CODE DELETED FOR %'
-      AND NOT procedure_dim.[Name] LIKE 'COPAYMENT % DOLLARS'
-      AND NOT procedure_dim.[Name] LIKE 'DELETED %'
-      -- Deliberately not obtaining empty CptCodes
       AND NOT procedure_dim.CptCode IN ('', '*Not Applicable', '*Deleted', '*Unknown')
--- todo: add the Avoid non-Clarity data added by Population Health condition
+      -- These appear to be invalid, deleted or non-procedure codes
+      AND NOT (procedure_dim.[Name] LIKE '-----%'
+               OR procedure_dim.[Name] LIKE 'CODE DELETED FOR %'
+               OR procedure_dim.[Name] LIKE 'DELETED %'
+               OR procedure_dim.[Name] LIKE 'COPAYMENT % DOLLARS')
+      -- Avoid non-Clarity data added by Population Health
+      AND procedure_dim._HasSourceClarity = 1 AND procedure_dim._IsDeleted = 0
+      -- Ensure procedure_dim.Code is a valid CPT4 code
+      AND concept.vocabulary_id = 'CPT4'
+      AND concept.concept_code  = procedure_dim.Code
 GROUP BY procedure_dim.ProcedureEpicId,
          procedure_dim.[Name],
+         concept.concept_name,
          procedure_dim.CptCode;
 
-/*
-Use this to determine the differences between the results of these two SELECTS:
-SELECT * FROM TableA
-UNION
-SELECT * FROM TableB
-EXCEPT
-SELECT * FROM TableA
-INTERSECT
-SELECT * FROM TableB
-*/
+-- Conflicting codes from the two methods, which can be reconciled or ignored
+-- Join on ProcedureEpicId, showing both Code and CptCode
+IF OBJECT_ID(N'tempdb..#conflicting_proc_mappings') IS NOT NULL
+	DROP TABLE #conflicting_proc_mappings;
+
+SELECT #proc_mappings_from_cpt_code.ProcedureEpicId,
+       #proc_mappings_from_cpt_code.Epic_name,
+       #proc_mappings_from_cpt_code.Code AS 'CptCode_code',
+       #proc_mappings_from_code.Code AS 'Code_code'
+INTO #conflicting_proc_mappings
+FROM #proc_mappings_from_cpt_code,
+     #proc_mappings_from_code
+WHERE #proc_mappings_from_cpt_code.ProcedureEpicId = #proc_mappings_from_code.ProcedureEpicId
+      AND #proc_mappings_from_cpt_code.Code <> #proc_mappings_from_code.Code;
+
+SELECT *
+FROM #conflicting_proc_mappings;
+
+-- Code mappings that do not conflict or are provided by only one method, which will be used as is
+IF OBJECT_ID(N'tempdb..#non_conflicting_proc_mappings') IS NOT NULL
+	DROP TABLE #non_conflicting_proc_mappings;
+
+-- Mappings from CptCode and Code
+SELECT *
+INTO #non_conflicting_proc_mappings
+FROM (SELECT *
+      FROM #proc_mappings_from_cpt_code
+      WHERE ProcedureEpicId NOT IN (SELECT ProcedureEpicId
+                                    FROM #conflicting_proc_mappings)
+
+      UNION
+
+      SELECT *
+      FROM #proc_mappings_from_code
+      WHERE ProcedureEpicId NOT IN (SELECT ProcedureEpicId
+                                    FROM #conflicting_proc_mappings)) table_alias;
+
+SELECT *
+FROM #non_conflicting_proc_mappings;
+
 /*
 -- Despite Tim's note "Sharon determined that the numeric-only SurgicalProcedureEpicId values
 -- (ie, the ones that do not start with “M”) almost always contain the CPT code embedded in the 4th through 8th position",
